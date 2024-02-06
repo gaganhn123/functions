0cd3011bcf1059d05562290d2c7f962204621b9f
gaganhn123 <149966959+gaganhn123@users.noreply.github.com>
2024-02-06 23:51:29 +0530

    Update functions.sol

--- functions.sol
+++ functions.sol
@@ -2,16 +2,20 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts@5.0.1/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts@5.0.1/token/ERC20/extensions/ERC20Burnable.sol";
+
 import "@openzeppelin/contracts@5.0.1/access/Ownable.sol";
 
-contract Gagan is ERC20, ERC20Burnable, Ownable {
+contract Gagan is ERC20,  Ownable {
     constructor(address initialOwner) ERC20("gagan", "G") Ownable(initialOwner) {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     } 
-    // function burn(uint256 burn_value)public virtual override {
-    //     _burn(msg.sender,burn_value);
-    // }
+    function burn(uint256 burn_value)public {
+         _burn(msg.sender,burn_value);
+    }
+    function transfer(address transf_addr, uint256 transf_value)public override returns(bool) {
+        _transfer(msg.sender,transf_addr, transf_value);
+        return true;
+    }
 }
